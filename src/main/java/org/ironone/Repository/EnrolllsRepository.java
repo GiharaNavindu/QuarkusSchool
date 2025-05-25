35820d9ab915c2c522a2c19455d7ba1274febfac
GiharaNavindu <raviherath1961@gamil.com>
2025-05-25 21:59:46 +0530

    EnrollsRepo is added

---
+++ src/main/java/org/ironone/Repository/EnrolllsRepository.java
@@ -0,0 +1,33 @@
+package org.ironone.Repository;
+import org.ironone.Entity.Enrolls;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import java.util.List;
+
+public class EnrolllsRepository implements PanacheRepository<Enrolls> {
+
+    // Method to find an enrollment by its ID
+    public Enrolls findById(Long id) {
+        return find("enrollmentId", id).firstResult();
+    }
+
+    // Method to retrieve all enrollments
+    public List<Enrolls> findAllEnrollments() {
+        return listAll();
+    }
+
+    // Method to save a new enrollment
+    public void save(Enrolls enrolls) {
+        persist(enrolls);
+    }
+
+    // Method to update an existing enrollment
+    public void update(Enrolls enrolls) {
+        persist(enrolls);
+    }
+
+    // Method to delete an enrollment by its ID
+    public void delete(Long id) {
+        delete("enrollmentId", id);
+    }
+}
